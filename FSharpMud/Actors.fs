6161c03d30c9342d28a91115ba7cfd30ef9ab8e0
rogeralsing <rogeralsing@gmail.com>
2015-09-25 16:15:28 +0200

    !

--- FSharpMud/Actors.fs
+++ FSharpMud/Actors.fs
@@ -6,41 +6,52 @@ open Messages
 open MessageHandlers
 open ActorState
 
-let emptySet = Set.empty<NamedObject>
-let nobody = ActorRefs.Nobody :> IActorRef
-let namedNobody = { name = "void"; ref = nobody }
-let emptyState = { container = namedNobody
-                   output = nobody
-                   objectsYouHave = emptySet
-                   objectsYouSee = emptySet
-                   exitsYouHave = emptySet
-                   exitsYouSee = emptySet }
+let emptyState = 
+    let emptySet = Set.empty<NamedObject>
+    let nobody = ActorRefs.Nobody :> IActorRef
+    
+    let namedNobody = 
+        { name = "void"
+          ref = nobody }
+    { container = namedNobody
+      output = nobody
+      objectsYouHave = emptySet
+      objectsYouSee = emptySet
+      exitsYouHave = emptySet
+      exitsYouSee = emptySet }
 
 let container (name : string) (mailbox : Actor<obj>) = 
-    let namedSelf = { name = name; ref = mailbox.Self }
+    let namedSelf = 
+        { name = name
+          ref = mailbox.Self }
     
     let rec loop (state : ThingState) = 
         actor { 
             let! m = mailbox.Receive()
             match m with
-            | :? ContainerMessage as message -> return! containerHandler message namedSelf loop state 
-            | :? ContainedMessages as message -> return! containedHandler message namedSelf loop state 
-            | unhandled -> mailbox.Unhandled unhandled              
+            | :? ContainerMessage as message -> return! containerHandler message namedSelf loop state
+            | :? ContainedMessages as message -> return! containedHandler message namedSelf loop state
+            | :? NotifyMessages as message -> return! notifyHandler message namedSelf loop state
+            | :? ThingMessage as message -> return! thingHandler message namedSelf loop state
+            | unhandled -> mailbox.Unhandled unhandled
         }
+    
     loop emptyState
 
 let living (name : string) (mailbox : Actor<obj>) = 
-    let namedSelf = { name = name; ref = mailbox.Self }
+    let namedSelf = 
+        { name = name
+          ref = mailbox.Self }
     
     let rec loop (state : ThingState) = 
         actor { 
             let! m = mailbox.Receive()
             match m with
-            | :? ContainerMessage as message -> return! containerHandler message namedSelf loop state 
-            | :? ContainedMessages as message -> return! containedHandler message namedSelf loop state 
-            | :? NotifyMessages as message -> return! notifyHandler message namedSelf loop state             
+            | :? ContainerMessage as message -> return! containerHandler message namedSelf loop state
+            | :? ContainedMessages as message -> return! containedHandler message namedSelf loop state
+            | :? NotifyMessages as message -> return! notifyHandler message namedSelf loop state
             | :? ThingMessage as message -> return! thingHandler message namedSelf loop state
-            | unhandled -> mailbox.Unhandled unhandled              
+            | unhandled -> mailbox.Unhandled unhandled
         }
-
+    
     loop emptyState
